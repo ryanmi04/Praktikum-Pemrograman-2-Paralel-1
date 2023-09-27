8b205ef741b1325f2ae036259038d696f8aa220c
Ryan MI <ryanmuhammad2004irfanm@gmail.com>
2023-09-27 15:04:34 +0700

    commit

--- PRAKTIKUM 1/src/PRAK104_2210817310013_RyanMuhammadIrfan.java
+++
@@ -1,7 +0,0 @@
-public class PRAK104_2210817310013_RyanMuhammadIrfan {
-    public static void main(String[] args) {
-        String nama = "Ryan muhammad Irfan";
-        int umur = 18;
-        System.out.println("Perkenalkan, Nama Saya " + nama + ", Umur Saya " + umur);
-    }
-}
