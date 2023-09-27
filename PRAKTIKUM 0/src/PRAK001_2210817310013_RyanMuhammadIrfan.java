3722f316f62296fb31ce11ca2f12189ed078d29b
Ryan MI <ryanmuhammad2004irfanm@gmail.com>
2023-09-27 15:04:59 +0700

    commit

---
+++ PRAKTIKUM 0/src/PRAK001_2210817310013_RyanMuhammadIrfan.java
@@ -0,0 +1,8 @@
+public class PRAK101_2210817310013_RyanMuhammadIrfan {
+    public static void main(String[] args){
+
+        System.out.println("Selamat Pagi, Ryan Muhammad Irfan");
+        System.out.println("Selamat Siang, Ryan Muhammad Irfan");
+        System.out.println("Selamat Malam, Ryan Muhammad Irfan");
+    }
+}
---
+++ PRAKTIKUM 0/src/PRAK002_2210817310013_RyanMuhammadIrfan.java
@@ -0,0 +1,10 @@
+public class PRAK102_2210817310013_RyanMuhammadIrfan {
+    public static void main(String[] args) {
+        System.out.println("##############################");
+        System.out.println("#                            #");
+        System.out.println("#     Ryan Muhammad Irfan    #");
+        System.out.println("#        2210817310013       #");
+        System.out.println("#                            #");
+        System.out.println("##############################");
+    }
+}
