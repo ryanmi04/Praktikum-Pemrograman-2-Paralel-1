046a5476be735864333373b1f6479d2bcc43d20c
Ryan MI <ryanmuhammad2004irfanm@gmail.com>
2023-10-17 21:02:18 +0700

    commit

--- PRAKTIKUM 2/PRAK201-2210817310013-RyanMuhammadIrfan/src/Soal1Main.java
+++ PRAKTIKUM 2/PRAK201-2210817310013-RyanMuhammadIrfan/src/Soal1Main.java
@@ -1,12 +1,12 @@
 public class Soal1Main {
     public static void main(String[] args) {
-        Buah buah1 = new Buah("Apel", 0.4,7000.0,40.0);
+        Buah buah1 = new Buah("Apel", 0.4,7000, 40);
         buah1.info();
 
-        Buah buah2 = new Buah("Mangga", 0.2, 3500.0, 15.0);
+        Buah buah2 = new Buah("Mangga", 0.2, 3500, 15);
         buah2.info();
 
-        Buah buah3 = new Buah("Alpukat", 0.25, 10000.0, 12.0);
+        Buah buah3 = new Buah("Alpukat", 0.25, 10000, 12);
         buah3.info();
     }
 }
