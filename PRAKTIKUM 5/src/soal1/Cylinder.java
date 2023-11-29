af5200b7a5aad540b096a85f6e7e974aa03f5a7e
Ryan MI <ryanmuhammad2004irfanm@gmail.com>
2023-11-29 08:03:10 +0700

    commit

---
+++ PRAKTIKUM 5/src/soal1/Cylinder.java
@@ -0,0 +1,21 @@
+package soal1;
+
+public class Cylinder extends Shape
+{
+    private double radius;
+    private double height;
+    public Cylinder(double r , double h)
+    {
+        super("Cylinder");
+        radius = r;
+        height = h;
+    }
+    public double area()
+    {
+        return Math.PI * (radius * radius)  * height;
+    }
+    public String toString()
+    {
+        return super.toString() + " of radius " + radius + " and height " + height;
+    }
+}
