7d4cd24be8510bed2e7d1f324f44a8323a1fdca2
ryanmi04 <114926790+ryanmi04@users.noreply.github.com>
2023-12-12 22:01:59 +0700

    commit

---
+++ PRAKTIKUM6/src/main/java/com/example/praktikum6/Mahasiswa.java
@@ -0,0 +1,26 @@
+package com.example.praktikum6;
+
+public class Mahasiswa {
+    private String nama, nim;
+
+    public Mahasiswa(String nama, String nim) {
+        this.nama = nama;
+        this.nim = nim;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+}
